1b3a1f94c8ae5daab0513b97cb37a08c62333b6c
Naim Kabir <kabir.naim@gmail.com>
2021-02-06 01:34:03 -1000

    [Server] Docker container with node, npm deps, and compiled typscript (#2)
    * Added initial Hello World functionality!
    
    This features a Dockerfile that creates an image that installs deps, compiles some Typescript, and runs a server.
    
    * adding newlns and also some comments

---
+++ server/src/app.ts
@@ -0,0 +1,14 @@
+import * as express from "express"
+
+const app = express()
+const port: number = 4000
+
+// Default endpoint
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Serve
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
