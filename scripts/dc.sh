f9f31f0bdc6e7d5005aac990536c71c0268096df
NaimKabir <kabir.naim@gmail.com>
2021-02-07 23:22:57 -1000

    docs on wiki_set_up.sql process

--- scripts/dc.sh
+++ scripts/dc.sh
@@ -10,6 +10,7 @@ then
     cd server; docker-compose --file docker-compose.yml up --detach; cd -
 
     # restore wiki set-up tables
+    # process on how I stored them recorded here: https://github.com/NaimKabir/ethical-consumption/pull/12
     echo "Constructing up Wiki tables..."
     docker cp -L server/data/wiki_set_up.sql database:.
     docker exec database bash -c "psql -U postgres -d default < wiki_set_up.sql"
