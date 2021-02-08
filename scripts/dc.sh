776a44b69524544b1d62b2da09c7f1f72719f794
Naim Kabir <kabir.naim@gmail.com>
2021-02-07 23:27:45 -1000

    [Hack: Server] Set up postgres tables for Wiki so dc up 'installs' it (#12)
    * Set up postgres tables for Wiki so dc up 'installs' it
    
    * docs on wiki_set_up.sql process

--- scripts/dc.sh
+++ scripts/dc.sh
@@ -8,6 +8,13 @@ arg=$1
 if [ "$arg" == "up" ];
 then
     cd server; docker-compose --file docker-compose.yml up --detach; cd -
+
+    # restore wiki set-up tables
+    # process on how I stored them recorded here: https://github.com/NaimKabir/ethical-consumption/pull/12
+    echo "Constructing up Wiki tables..."
+    docker cp -L server/data/wiki_set_up.sql database:.
+    docker exec database bash -c "psql -U postgres -d default < wiki_set_up.sql"
+
     exit 0
 fi
 
